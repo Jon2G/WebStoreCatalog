ef276dc88900b71da774bf196f1ae15a3075f9f0
Jon2G <jonyjouna@gmail.com>
2021-12-13 20:09:00 -0600

    Cargar imagenes

---
+++ Data/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class Extensions
+    {
+        public static async Task<string> ToImage64(this Stream selectedFileData)
+        {
+            StringBuilder base64 = new StringBuilder("data:png;base64,");
+            using (Stream stream = selectedFileData)
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    await stream.CopyToAsync(memoryStream);
+                    byte[] data = memoryStream.ToArray();
+                    return data.ToImage64();
+                }
+            }
+        }
+        public static string ToImage64(this byte[] data)
+        {
+            StringBuilder base64 = new StringBuilder("data:png;base64,");
+            base64.Append(Convert.ToBase64String(data));
+            return base64.ToString();
+        }
+    }
+}
